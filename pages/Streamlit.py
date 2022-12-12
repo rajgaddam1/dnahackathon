e55e4699f365ab16b089bce811d8b5d560b0c335
rajgaddam1 <119797412+rajgaddam1@users.noreply.github.com>
2022-12-12 12:43:50 +0530

    Update Streamlit.py

--- pages/Streamlit.py
+++ pages/Streamlit.py
@@ -168,7 +168,7 @@ if sel_data != 'Select below available Databases':
 ###Homepage######
 
 if sel_data == 'Select below available Databases' and sel_ware == 'Select below available wareshouse':
-    st.title("Snowflake Hackathon")
+    st.subheader("Snowflake Hackathon")
     image = Image.open('Infosys_logo.JPG')
     st.write('This Application is created to To connect to snowflake database from streamlit UI It is used to Create Warehouse/Database/Schema/Table in Snowflake')
 
