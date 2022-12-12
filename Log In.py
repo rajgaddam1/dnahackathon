1634a351edebd82226548446f80970150a702e0b
ragu18 <raju1997.gr@gmail.com>
2022-12-12 11:57:24 +0530

    first commit

---
+++ Log In.py
@@ -0,0 +1,70 @@
+import streamlit as st
+import os
+import snowflake.connector
+import warnings
+warnings.filterwarnings("ignore")
+
+user = os.environ.get('user')
+password = os.environ.get('password')
+account = os.environ.get('account')
+
+st.set_page_config(
+    page_title="Sign in to Snowflake ❄️",
+    page_icon="👋",
+)
+
+st.title("Sign in to Snowflake ❄️")
+
+
+
+def switch_page(page_name: str):
+    from streamlit.runtime.scriptrunner import RerunData, RerunException
+    from streamlit.source_util import get_pages
+
+    def standardize_name(name: str) -> str:
+        return name.lower().replace("_", " ")
+
+    page_name = standardize_name(page_name)
+
+    pages = get_pages("app.py")  # OR whatever your main page is called
+
+    for page_hash, config in pages.items():
+        if standardize_name(config["page_name"]) == page_name:
+            raise RerunException(
+                RerunData(
+                    page_script_hash=page_hash,
+                    page_name=page_name,
+                )
+            )
+
+    page_names = [standardize_name(config["page_name"]) for config in pages.values()]
+
+    raise ValueError(f"Could not find page {page_name}. Must be one of {page_names}")
+    
+def intro():
+    
+    #st.header("Sign in to Snowflake ❄️", anchor=None)
+
+    user_name1 = st.text_input('Enter User Name to connect to Snowflake DB',label_visibility="visible")
+
+    account_name = st.text_input('Enter Account Identifier of Snowflake',label_visibility="visible")
+
+    password1 = st.text_input('Enter password to connect to Snowflake DB',label_visibility="visible",type='password')
+    #agree = st.button('Submit')
+    if st.button("submit"):
+        if user == user_name1 and password == password1 and account == account_name:
+            
+            st.write('Logged in Successfully')
+            switch_page("Streamlit")
+
+        else:
+            st.write('Invalid Username or Password')
+
+page_names_to_funcs = {
+    "Log in to snowflake": intro,
+}
+demo_name = st.sidebar.selectbox("Choose a action", page_names_to_funcs.keys())
+
+page_names_to_funcs[demo_name]()
+       
+                
---
+++ pages/Streamlit.py
@@ -0,0 +1,179 @@
+import streamlit as st
+import os
+import snowflake.connector
+import warnings
+warnings.filterwarnings("ignore")
+import pandas as pd
+from snowflake.connector.connection import SnowflakeConnection
+
+
+##To manage bug in sreamlit(Intialize button click)
+if 'key' not in st.session_state:
+    st.session_state.key = False
+
+def callback():
+    st.session_state.key = True
+    
+###Function to convert data to csv
+
+@st.cache
+def convert_df(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return df.to_csv().encode('utf-8')
+
+##############Snowflake Credentials
+user = os.environ.get('user')
+password = os.environ.get('password')
+account = os.environ.get('account')
+
+###Snow connection
+
+con = snowflake.connector.connect(
+                    user = user,
+                    password = password,
+                    account='MD93775.ap-southeast-1')
+
+####Snowflake connection
+def get_connector() -> SnowflakeConnection:
+    """Create a connector to SnowFlake using credentials filled in Streamlit secrets"""
+    con = snowflake.connector.connect(
+    user = user,
+    password = password,
+    account = account,
+    warehouse='DNAHACK')
+    return con
+
+snowflake_connector = get_connector()
+#####Show warehouses
+def get_wareshouse(_connector) -> pd.DataFrame:
+    return pd.read_sql("SHOW WAREHOUSES;", _connector)
+
+wareshouse = get_wareshouse(snowflake_connector)
+
+list_ware = wareshouse['name'].to_list()
+list_up = ['Select below available wareshouse']
+list_ware_up = list_up + list_ware
+
+##Snowflake Waarehouse dataframe to csv
+
+ware_csv = convert_df(wareshouse)
+
+#################Function to create Warehouse
+
+def create_ware(con):
+    ware_name = st.text_input('Enter Warehouse Name')
+    ware_size = st.select_slider('Select size', ['XSMALL', 'SMALL', 'MEDIUM', 'LARGE', 'XLARGE', 'XXLARGE', 'XXXLARGE', 'X4LARGE', 'X5LARGE', 'X6LARGE'])
+    sql_cmd = 'CREATE OR REPLACE WAREHOUSE  ' + str(ware_name) + ' ' +'WAREHOUSE_SIZE = '+ str(ware_size) +';'
+    if st.button('Create Warehouse'):
+        try:
+            cur = con.cursor()
+            cur.execute(sql_cmd)
+            st.write('Warehouse has been created')
+        except Exception as e:
+            print(e)
+            st.write('An error has occured please check logs')
+        finally:
+            cur.close()
+        con.close()
+#################Function to create Databsase
+def create_data(con):
+    database_name = st.text_input('Enter Database Name')
+    database_type = st.radio('Select Database Type', ['PERMANENT', 'TRANSIENT'])
+    if database_type == 'PERMANENT':
+        sql_cmd = 'CREATE OR REPLACE DATABASE  ' + str(database_name) + ';'
+    else:
+        sql_cmd = 'CREATE OR REPLACE TRANSIENT DATABASE  ' + str(database_name) + ';'
+        
+    if st.button('Create Database'):
+        try:
+            cur = con.cursor()
+            cur.execute(sql_cmd)
+            st.write('Database has been created')
+        except Exception as e:
+            print(e)
+            st.write('An error has occured please check logs')
+        finally:
+            cur.close()
+        con.close()
+        
+    
+################SIDEBAR_1(WAREHOUSE)
+with st.sidebar:
+    sel_ware = st.radio(
+        "Warehouse",
+        list_ware_up
+    )
+
+if sel_ware != 'Select below available wareshouse':
+    if st.button('Create a new warehouse', on_click = callback) or st.session_state.key:
+        
+        create_ware(con)
+        #pass
+    st.subheader('Warehouse Information')
+
+    st.dataframe(wareshouse[['name', 'size']].loc[wareshouse['name'] == sel_ware])
+    
+    st.markdown("Click on below button to Download full Information about Warehouse")
+    st.download_button(
+    label = "Download data as CSV",
+    data = ware_csv,
+    file_name = 'Warehouse_info.csv',
+    mime = 'text/csv',
+)
+
+
+####ShowDatabases
+def get_databases(_connector) -> pd.DataFrame:
+    return pd.read_sql("SHOW DATABASES;", _connector)
+
+databases = get_databases(snowflake_connector)
+
+##Snowflake Waarehouse dataframe to csv
+
+database_csv = convert_df(databases)
+
+##Adding Database type by creating copy of dataframe
+databases_up = databases.copy()
+databases_up.rename(columns={'options': 'type'}, inplace=True)
+#databases_up['type'] = databases_up['type'].replace(np.nan, 'PERMANENT')
+databases_up.type.fillna("PERMANENT",inplace = True)
+
+
+list_data = databases['name'].to_list()
+list_up = ['Select below available Databases']
+list_data_up = list_up + list_data
+#############SIDEBAR_2(DATABASES)
+with st.sidebar:
+    global sel_data
+    sel_data = st.radio("Databases", list_data_up)
+    
+if sel_data != 'Select below available Databases':
+    if st.button('Create a Database', on_click = callback) or st.session_state.key:
+        
+        create_data(con)
+        #pass
+    st.subheader('Database Information')
+
+    st.dataframe(databases_up[['name', 'type']].loc[databases_up['name'] == sel_data])
+    
+    st.markdown("Click on below button to Download full Information about Database")
+    st.download_button(
+    label = "Download data as CSV",
+    data = database_csv,
+    file_name = 'Database_info.csv',
+    mime = 'text/csv',
+)
+    
+#############SIDEBAR_3(Schemas)  
+def get_schema(_connector, dbname) -> pd.DataFrame:
+    sql_cmd2 = 'SHOW SCHEMAS IN DATABASE ' + str(dbname) + ';'
+    return pd.read_sql(sql_cmd2, _connector)
+ 
+if sel_data != 'Select below available Databases':
+    global sel_schema
+    schemas_df = get_schema(snowflake_connector, sel_data)
+    sc_list_data = schemas_df['name'].to_list()
+    sc_list_up = ['Select below available Schemas']
+    sc_list_data_up = sc_list_up + sc_list_data
+    with st.sidebar:
+        sel_schema = st.radio("Schema",sc_list_data_up)
