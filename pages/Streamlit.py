0b9d535d09f19166c1759802f3cfa566209d8d8c
rajgaddam1 <119797412+rajgaddam1@users.noreply.github.com>
2022-12-15 18:15:18 +0530

    Update Streamlit.py

--- pages/Streamlit.py
+++ pages/Streamlit.py
@@ -453,11 +453,13 @@ if sel_data != 'Create a Database' and sel_data !=  '-------------------':
             ###############Copy Query
             agree3 = st.checkbox('Copy query from existing Table')
             if agree3:
+                st.success('Success')
                 
-                tables_df_query = get_table(snowflake_connector, sel_data, sel_schema)
-                show_query(con,sel_data,sel_schema,tables_df_query)
-                query_df = show_query(snowflake_connector,sel_data,sel_schema, tables_df_query)
-                st.dataframe(query_df)
+                
+                #tables_df_query = get_table(snowflake_connector, sel_data, sel_schema)
+                #show_query(con,sel_data,sel_schema,tables_df_query)
+                #query_df = show_query(snowflake_connector,sel_data,sel_schema, tables_df_query)
+                #st.dataframe(query_df)
             
             create_table(con)
     else:
