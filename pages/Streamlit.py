0b045bd639b0d06cc055c6525d90b8d5f65e7291
rajgaddam1 <119797412+rajgaddam1@users.noreply.github.com>
2022-12-15 23:46:53 +0530

    Update Streamlit.py

--- pages/Streamlit.py
+++ pages/Streamlit.py
@@ -567,8 +567,8 @@ with st.sidebar:
     sql_window = st.checkbox('SQL Window')
     if sql_window:
         st.title('SNOWFLAKE CLIENT')
-        sel_role2 = st.selectbox("Role", ['DCM_USER', 'ACCOUNTADMIN', 'ORGADMIN', 'PUBLIC', 'SYSADMIN'])
-        sel_ware2 = st.selectbox("User", ['DNAHCK_W', 'DNAHACK','SNOWFLAKE'])
+        #sel_role2 = st.selectbox("Role", ['DCM_USER', 'ACCOUNTADMIN', 'ORGADMIN', 'PUBLIC', 'SYSADMIN'])
+        #sel_ware2 = st.selectbox("User", ['DNAHCK_W', 'DNAHACK','SNOWFLAKE'])
         sql_query1 = st.text_input('Enter SQL')
         if st.button('Enter SQL'):
             pass
@@ -577,7 +577,7 @@ with st.sidebar:
 
     
 #######HOME PAGE
-if sel_ware == '-------------------' and sel_data == '-------------------' and sel_role == '-------------------'  and sel_user == '-------------------' and sel_report == '-------------------' and not sql_window:
+if sel_ware == '-------------------' and sel_data == '-------------------' and sel_role == '-------------------'  and sel_user == '-------------------' and sel_report == '-------------------':
     st.title('SNOWFLAKE CLIENT')
     sel_role1 = st.selectbox("Role", ['DCM_USER', 'ACCOUNTADMIN', 'ORGADMIN', 'PUBLIC', 'SYSADMIN'])
     sel_ware1 = st.selectbox("User", ['DNAHCK_W', 'DNAHACK','SNOWFLAKE'])
